475f1197299807acb074bee7555a4aad18a5451d
Eternal-Rise <my_dawn@ukr.net>
2024-02-12 15:47:39 +0200

    chore: move styles to assets

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import 'vfonts/Lato.css';
-import './styles/index.css';
+import './assets/styles/index.css';
 import App from './App.vue';
 
 const app = createApp(App);
